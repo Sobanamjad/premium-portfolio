c2a53da03146ec13aee31f80a209c30760942e89
Sobanamjad <sobanamjad097@gmail.com>
2026-07-13 14:31:39 +0500

    feat: add favicon icon

--- src/data/socialLinks.js
+++ src/data/socialLinks.js
@@ -1,9 +1,9 @@
 import { FiGithub, FiLinkedin, FiTwitter, FiDribbble, FiMail } from 'react-icons/fi'
 
 export const socialLinks = [
-  { id: 1, name: 'GitHub', icon: FiGithub, url: 'https://github.com/yourusername' },
-  { id: 2, name: 'LinkedIn', icon: FiLinkedin, url: 'https://linkedin.com/in/yourusername' },
+  { id: 1, name: 'GitHub', icon: FiGithub, url: 'https://github.com/' },
+  { id: 2, name: 'LinkedIn', icon: FiLinkedin, url: 'www.linkedin.com/in/soban-amjad-18381b315' },
   { id: 3, name: 'Twitter', icon: FiTwitter, url: 'https://twitter.com/yourusername' },
   { id: 4, name: 'Dribbble', icon: FiDribbble, url: 'https://dribbble.com/yourusername' },
-  { id: 5, name: 'Email', icon: FiMail, url: 'mailto:hello@yourdomain.com' },
+  { id: 5, name: 'Email', icon: FiMail, url: 'mailto:soobanamjad097@gmail.com' },
 ]
