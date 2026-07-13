f0c2b98f0c8d67a59deb54826c1e6d5fc511a02e
Sobanamjad <sobanamjad097@gmail.com>
2026-07-13 14:43:19 +0500

    feat:add skills

--- src/data/socialLinks.js
+++ src/data/socialLinks.js
@@ -2,8 +2,8 @@ import { FiGithub, FiLinkedin, FiTwitter, FiDribbble, FiMail } from 'react-icons
 
 export const socialLinks = [
   { id: 1, name: 'GitHub', icon: FiGithub, url: 'https://github.com/' },
-  { id: 2, name: 'LinkedIn', icon: FiLinkedin, url: 'www.linkedin.com/in/soban-amjad-18381b315' },
+  { id: 2, name: 'LinkedIn', icon: FiLinkedin, url: 'https://www.linkedin.com/in/soban-amjad-18381b315/' },
   { id: 3, name: 'Twitter', icon: FiTwitter, url: 'https://twitter.com/yourusername' },
-  { id: 4, name: 'Dribbble', icon: FiDribbble, url: 'https://dribbble.com/yourusername' },
+  // { id: 4, name: 'Dribbble', icon: FiDribbble, url: 'https://dribbble.com/yourusername' },
   { id: 5, name: 'Email', icon: FiMail, url: 'mailto:soobanamjad097@gmail.com' },
 ]
